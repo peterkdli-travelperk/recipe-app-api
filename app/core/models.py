aaeaf353c3e52e2bc7a5130b4005cfcdaf149004
peterkdli-travelperk <peter.kentish@travelperk.com>
2022-05-09 15:57:40 +0200

    add super user creation

--- app/core/models.py
+++ app/core/models.py
@@ -6,11 +6,23 @@ class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
         """Create and save new user"""
+        if not email:
+            raise ValueError('Users must have an email ')
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+        """Create super user and saves"""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+
+        user.save(using=self._db)
+        return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """Custom user model with email and pw"""
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -22,3 +22,19 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(email, 'testpassword')
 
         self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'testpassword')
+
+    def test_create_new_superuser(self):
+        """Test creating new superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            "test@peter.com",
+            'test123'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
