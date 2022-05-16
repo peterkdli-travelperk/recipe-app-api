f1454cec7bd795138a1c51e40b7a21911433610d
peterkdli-travelperk <peter.kentish@travelperk.com>
2022-05-16 15:11:39 +0200

    add ingredients api

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from core.models import Tag
+from core.models import Tag, Ingredient
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -11,3 +11,9 @@ class TagSerializer(serializers.ModelSerializer):
         fields = ('id', 'name')
         read_only_fields = ('id',)
 
+
+class IngredientSerializer(serializers.ModelSerializer):
+   class Meta:
+       model = Ingredient
+       fields = ('id', 'name')
+       read_only_fields= ('id',)
