7641c211d8944e68bc929aa7eaba1dc67f0c0771
peterkdli-travelperk <peter.kentish@travelperk.com>
2022-05-16 17:50:42 +0200

    add recipe creation

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -43,3 +43,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
